edf9528d3d2ef6a88eeca9d7f75f833feaf5dac4
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-08-19 00:23:18 -0700

    Update website

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var hints = document.getElementsByClassName('hint');
+var profilePicture = document.getElementsByClassName('picture-container')[0];
+var introText = document.getElementsByClassName('text-container')[0];
+var ageCounter = document.getElementById('age-counter');
+var githubHint = document.getElementsByClassName('small')[0];
+
+var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ff0000"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ff0000","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
+
+setInterval(function () {
+  calcAge('2001-01-30')
+}, 50);
+
+document.addEventListener('scroll', function () {
+  for (var i = 0; i < hints.length; i++) {
+    var hint = hints[i];
+    hint.style.opacity = 1 - document.body.scrollTop / 1000 + 0.2 + i;
+  }
+
+  profilePicture.style.opacity = 1 - document.body.scrollTop / 250 + 0.5;
+  introText.style.opacity = 1 - document.body.scrollTop / 250 + 0.5;
+  githubHint.style.opacity = document.body.scrollTop / 500 - 4.2;
+});
+
+function calcAge(dateString) {
+  var birthday = new Date(dateString);
+  ageCounter.innerHTML = ((Date.now() - birthday) / (31557600000)).toFixed(8);
+}
+
+particlesJS('particles-js', particleSettings);
