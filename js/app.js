057d9ff7b22484092c9293b3747bfe59ec55b572
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-08-19 18:58:54 -0700

    Fix social icons

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
-var hints = document.getElementsByClassName('hint');
-var profilePicture = document.getElementsByClassName('picture-container')[0];
-var introText = document.getElementsByClassName('text-container')[0];
-var ageCounter = document.getElementById('age-counter');
+var hints = $('.hint');
+var profilePicture = $('.picture-container')[0];
+var introText = $('.text-container')[0];
+var ageCounter = $('#age-counter');
 
 //var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ff0000"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ff0000","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
 var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
+var birthday = '2001-01-30';
 
 setInterval(function () {
-  calcAge('2001-01-30')
-}, 50);
+    calcAge(birthday);
+}, 250);
 
-document.addEventListener('scroll', function () {
-  for (var i = 0; i < hints.length; i++) {
-    var hint = hints[i];
-    hint.style.opacity = 1 - document.body.scrollTop / 1000 + 0.2 + i;
-  }
+$(document).on('scroll', function () {
+    $.each(hints, function(i, hint) {
+        hint.style.opacity = 1 - $(document).scrollTop() / 1000 + 0.2 + i;
+    });
 
-  profilePicture.style.opacity = 1 - document.body.scrollTop / 300 + 0.5;
-  introText.style.opacity = 1 - document.body.scrollTop / 300 + 0.5;
+    profilePicture.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
+    introText.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
 });
 
+calcAge(birthday);
 function calcAge(dateString) {
-  var birthday = new Date(dateString);
-  ageCounter.innerHTML = ((Date.now() - birthday) / (31557600000)).toFixed(8);
+    var b = new Date(dateString);
+    ageCounter.text(((Date.now() - b) / (31557600000)).toFixed(8));
 }
 
 particlesJS('section-1', particleSettings);
