be842b18e193cf30558df63f503215ea8d0f28b8
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-08-21 00:01:55 -0700

    Redo mobile device checks

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ var profilePicture = $('.picture-container')[0];
 var introText = $('.text-container')[0];
 var ageCounter = $('#age-counter');
 
-//var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ff0000"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ff0000","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
-var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
+var isMobile = (screen && screen.width < 640);
+
+// Do not allow mobile devices to interact with the particles - it will cause random lag
+var particleSettings = {"particles":{"number":{"value":120,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":!isMobile,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":150,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
 var birthday = '2001-01-30';
 
 setInterval(function () {
@@ -12,12 +14,15 @@ setInterval(function () {
 }, 250);
 
 $(document).on('scroll', function () {
-    $.each(hints, function(i, hint) {
-        hint.style.opacity = 1 - $(document).scrollTop() / 1000 + 0.2 + i;
-    });
+    // Do not change opacity on mobile devices - it will cause elements to disappear too early
+    if (!isMobile) {
+        $.each(hints, function(i, hint) {
+            hint.style.opacity = 1 - $(document).scrollTop() / 1000 + 0.2 + i;
+        });
 
-    profilePicture.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
-    introText.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
+        profilePicture.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
+        introText.style.opacity = 1 - $(document).scrollTop() / 300 + 0.5;
+    }
 });
 
 calcAge(birthday);
