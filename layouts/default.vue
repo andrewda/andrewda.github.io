3033da4c20fba779b15b8e5897acab5bbe02ead5
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-01-02 12:12:38 -0800

    Add experience page

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,16 @@
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Montserrat', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
+  background-color: rgb(12, 12, 12);
+  color: #dff9fb;
   box-sizing: border-box;
 }
 
@@ -51,5 +53,27 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.title {
+  font-family: 'Montserrat', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 70px;
+  color: #526488;
+  letter-spacing: 1px;
+  animation-delay: 0.5s;
+}
+
+.subtitle {
+  font-family: 'Montserrat', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 300;
+  font-size: 35px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+  animation-delay: 1.5s;
 }
 </style>
